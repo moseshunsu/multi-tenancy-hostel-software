4fe8a744c46a903268ff20cf99f7df1a1fa928dd
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-04 11:43:38 +0100

    Initial commit

---
+++ src/main/java/net/hostelHub/MultiTenancyHostelSoftwareApplication.java
@@ -0,0 +1,13 @@
+package net.hostelHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenancyHostelSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenancyHostelSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/hostelHub/MultiTenancyHostelSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hostelHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenancyHostelSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
