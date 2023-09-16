f21ca5cb10d1a70a89d684c3b6b5761a4677b6e4
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-16 15:32:27 +0100

    Handled MethodArgumentNotValidException and IllegalArgumentException

--- src/main/java/net/hostelHub/advice/ApplicationExceptionHandler.java
+++ src/main/java/net/hostelHub/advice/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package net.hostelHub.advice;
 
 import net.hostelHub.exception.NoSuchElementException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +12,29 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errorMap.put(
+                        error.getField(), error.getDefaultMessage()
+                )
+        );
+        return errorMap;
+    }
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(NoSuchElementException.class)
     public Map<String, String> handleNoSuchElementException(NoSuchElementException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("errorMessage", ex.getMessage());
         return errorMap;
