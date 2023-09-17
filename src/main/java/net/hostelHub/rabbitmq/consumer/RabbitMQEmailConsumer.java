e7f7a7936ee6a4ca9009046c1f62ce83821c6831
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-17 22:15:36 +0100

    Adding build files

---
+++ src/main/java/net/hostelHub/rabbitmq/consumer/RabbitMQEmailConsumer.java
@@ -0,0 +1,20 @@
+package net.hostelHub.rabbitmq.consumer;
+
+//import net.hostelHub.email.dto.EmailDetails;
+//import net.hostelHub.email.service.EmailService;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+
+//@Service
+public class RabbitMQEmailConsumer {
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @RabbitListener(queues = "${rabbitmq.occupant.booking.queue}")
+//    public void sendUserRegistrationDetails(EmailDetails emailDetails) {
+//            emailService.sendSimpleMail(emailDetails);
+//    }
+
+}
