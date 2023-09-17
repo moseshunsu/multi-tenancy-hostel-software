03020f431244f99d4b48aa9817f2522333e2788c
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-17 23:03:32 +0100

    Added Cors Mappings

--- src/main/java/net/hostelHub/MultiTenancyHostelSoftwareApplication.java
+++ src/main/java/net/hostelHub/MultiTenancyHostelSoftwareApplication.java
@@ -2,9 +2,27 @@ package net.hostelHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MultiTenancyHostelSoftwareApplication {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*","")
+                        .allowedHeaders("*")
+                        .allowedMethods("POST","GET","PUT","PATCH","OPTIONS");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiTenancyHostelSoftwareApplication.class, args);
