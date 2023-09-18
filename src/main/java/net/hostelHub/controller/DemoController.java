0f888d6d6ff80d95fc33762fa9e4e544b2c0a347
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-18 01:37:29 +0100

    Hid demo controller

--- src/main/java/net/hostelHub/controller/DemoController.java
+++ src/main/java/net/hostelHub/controller/DemoController.java
@@ -1,11 +1,13 @@
 package net.hostelHub.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo")
+@Hidden
 public class DemoController {
 
     @GetMapping
